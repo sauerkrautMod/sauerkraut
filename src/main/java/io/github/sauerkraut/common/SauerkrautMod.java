d0ad0d720c5a54714ad7d7ba86c44cd65aac8fdf
busti <mbust@live.de>
2020-02-12 18:48:33 +0100

    optimize imports

--- src/main/java/io/github/sauerkraut/common/SauerkrautMod.java
+++ src/main/java/io/github/sauerkraut/common/SauerkrautMod.java
@@ -1,15 +1,8 @@
 package io.github.sauerkraut.common;
 
 import io.github.sauerkraut.common.init.ModBlocks;
-import net.minecraft.block.Block;
-import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.fml.event.server.FMLServerStartingEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
--- src/main/java/io/github/sauerkraut/common/init/ModBlocks.java
+++ src/main/java/io/github/sauerkraut/common/init/ModBlocks.java
@@ -3,15 +3,9 @@ package io.github.sauerkraut.common.init;
 import io.github.sauerkraut.common.SauerkrautMod;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
-import net.minecraftforge.event.RegistryEvent;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.RegistryObject;
-import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
-import net.minecraftforge.registries.IForgeRegistry;
-import net.minecraftforge.registries.ObjectHolder;
 
 public class ModBlocks {
     public static final DeferredRegister<Block> BLOCKS =
