03aa48fe4e58ed75917a8b8c3122101ea9122c5f
Plutor-29 <Dagobert-29>
2020-02-15 22:08:35 +0100

    Add copper_ingots, pointy_cabbage_seed, pointy_cabbage loot table, copper ore smelting

--- src/main/java/io/github/sauerkraut/common/init/ModItems.java
+++ src/main/java/io/github/sauerkraut/common/init/ModItems.java
@@ -1,6 +1,7 @@
 package io.github.sauerkraut.common.init;
 
 import io.github.sauerkraut.common.SauerkrautMod;
+import net.minecraft.item.BlockNamedItem;
 import net.minecraft.item.Item;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
@@ -11,5 +12,6 @@ public final class ModItems {
 
     public static final RegistryObject<Item> ITEM_SAUERKRAUT = ITEMS.register("sauerkraut",() -> new Item(new Item.Properties().group(ModItemGroups.MOD_ITEM_GROUP)));
     public static final RegistryObject<Item> ITEM_POINTY_CABBAGE = ITEMS.register("pointy_cabbage",() -> new Item(new Item.Properties().group(ModItemGroups.MOD_ITEM_GROUP)));
-
+    public static final RegistryObject<Item> ITEM_POINTY_CABBAGE_SEED = ITEMS.register("pointy_cabbage_seed", ModBlocks.BLOCK_POINTY_CABBAGE.lazyMap(block -> new BlockNamedItem(block, new Item.Properties().group(ModItemGroups.MOD_ITEM_GROUP))));
+    public static final RegistryObject<Item> ITEM_COPPER_INGOT = ITEMS.register("copper_ingot",() -> new Item(new Item.Properties().group(ModItemGroups.MOD_ITEM_GROUP)));
 }
