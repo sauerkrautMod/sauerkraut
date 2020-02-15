16643af21966f77dab57d4bbd33c76dff79a886c
convince18@posteo.de <Lolwurst.18>
2020-02-15 19:14:12 +0100

    added pointy cabbage block

---
+++ src/main/java/io/github/sauerkraut/common/block/ModCropsBlock.java
@@ -0,0 +1,9 @@
+package io.github.sauerkraut.common.block;
+
+import net.minecraft.block.CropsBlock;
+
+public class ModCropsBlock extends CropsBlock {
+    public ModCropsBlock(Properties builder){
+        super(builder);
+    }
+}
--- src/main/java/io/github/sauerkraut/common/init/ModBlocks.java
+++ src/main/java/io/github/sauerkraut/common/init/ModBlocks.java
@@ -1,6 +1,7 @@
 package io.github.sauerkraut.common.init;
 
 import io.github.sauerkraut.common.SauerkrautMod;
+import io.github.sauerkraut.common.block.ModCropsBlock;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 import net.minecraft.item.BlockItem;
@@ -14,6 +15,7 @@ public final class ModBlocks {
     public static final DeferredRegister<Item> ITEMS = new DeferredRegister<>(ForgeRegistries.ITEMS, SauerkrautMod.MODID);
 
     public static final RegistryObject<Block> BLOCK_COPPER_ORE = BLOCKS.register("copper_ore", () -> new Block(Block.Properties.create(Material.ROCK)));
+    public static final RegistryObject<Block> BLOCK_POINTY_CABBAGE = BLOCKS.register( "pointy_cabbage", () -> new ModCropsBlock(Block.Properties.create(Material.PLANTS)));
 
     private static final Item.Properties DEFAULT_ITEM_PROPS = new Item.Properties().group(ModItemGroups.MOD_ITEM_GROUP);
     public static final RegistryObject<Item> ITEM_COPPER_ORE = ITEMS.register("copper_ore", BLOCK_COPPER_ORE.lazyMap(block -> new BlockItem(block, DEFAULT_ITEM_PROPS)));
