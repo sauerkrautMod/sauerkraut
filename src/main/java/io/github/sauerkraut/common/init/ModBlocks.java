48826c4ca8c206827930809f1c3c60ec63737bd3
busti <mbust@live.de>
2020-02-16 02:25:33 +0100

    add commentary

--- src/main/java/io/github/sauerkraut/common/init/ModBlocks.java
+++ src/main/java/io/github/sauerkraut/common/init/ModBlocks.java
@@ -15,11 +15,20 @@ public final class ModBlocks {
     public static final DeferredRegister<Block> BLOCKS = new DeferredRegister<>(ForgeRegistries.BLOCKS, SauerkrautMod.MODID);
     public static final DeferredRegister<Item> ITEMS = new DeferredRegister<>(ForgeRegistries.ITEMS, SauerkrautMod.MODID);
 
+    // Ore Blocks
     private static final Block.Properties BLOCK_PROPS_ORE = Block.Properties.create(Material.ROCK).hardnessAndResistance(3);
     public static final RegistryObject<Block> BLOCK_COPPER_ORE = BLOCKS.register("copper_ore", () -> new ModOreBlock(BLOCK_PROPS_ORE, 0, 0));
+
+    // Plant Blocks
     public static final RegistryObject<Block> BLOCK_POINTY_CABBAGE = BLOCKS.register( "pointy_cabbage", () -> new ModCropsBlock(BLOCK_PROPS_ORE));
 
-    private static final Item.Properties ITEM_PROPS_DEFAULT = new Item.Properties().group(ModItemGroups.MOD_ITEM_GROUP);
-    public static final RegistryObject<Item> ITEM_COPPER_ORE = ITEMS.register("copper_ore", BLOCK_COPPER_ORE.lazyMap(block -> new BlockItem(block, ITEM_PROPS_DEFAULT)));
-    public static final RegistryObject<Item> ITEM_POINTY_CABBAGE_BLOCK = ITEMS.register("pointy_cabbage_block", BLOCK_POINTY_CABBAGE.lazyMap(block -> new BlockItem(block, ITEM_PROPS_DEFAULT)));
+    // Machine Blocks
+
+    private static final Item.Properties BLOCKITEM_PROPS_DEFAULT = new Item.Properties().group(ModItemGroups.MOD_ITEM_GROUP);
+
+    // Ore BlockItems
+    public static final RegistryObject<Item> ITEM_COPPER_ORE = ITEMS.register("copper_ore", BLOCK_COPPER_ORE.lazyMap(block -> new BlockItem(block, BLOCKITEM_PROPS_DEFAULT)));
+
+    // Plant BlockItems
+    public static final RegistryObject<Item> ITEM_POINTY_CABBAGE_BLOCK = ITEMS.register("pointy_cabbage_block", BLOCK_POINTY_CABBAGE.lazyMap(block -> new BlockItem(block, BLOCKITEM_PROPS_DEFAULT)));
 }
