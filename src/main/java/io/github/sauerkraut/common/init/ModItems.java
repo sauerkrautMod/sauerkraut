d1b85ddb35d6023043cf660f867de17e9ebd53a2
Plutor-29 <Dagobert-29>
2020-02-19 22:18:11 +0100

    Add iron_rod, copper_rod, draw_plate

--- src/main/java/io/github/sauerkraut/common/init/ModItems.java
+++ src/main/java/io/github/sauerkraut/common/init/ModItems.java
@@ -14,4 +14,12 @@ public final class ModItems {
     public static final RegistryObject<Item> ITEM_POINTY_CABBAGE = ITEMS.register("pointy_cabbage",() -> new Item(new Item.Properties().group(ModItemGroups.MOD_ITEM_GROUP)));
     public static final RegistryObject<Item> ITEM_POINTY_CABBAGE_SEED = ITEMS.register("pointy_cabbage_seed", ModBlocks.BLOCK_POINTY_CABBAGE.lazyMap(block -> new BlockNamedItem(block, new Item.Properties().group(ModItemGroups.MOD_ITEM_GROUP))));
     public static final RegistryObject<Item> ITEM_COPPER_INGOT = ITEMS.register("copper_ingot",() -> new Item(new Item.Properties().group(ModItemGroups.MOD_ITEM_GROUP)));
+    public static final RegistryObject<Item> ITEM_DRAW_PLATE = ITEMS.register("draw_plate",() -> new Item(new Item.Properties().group(ModItemGroups.MOD_ITEM_GROUP)));
+    public static final RegistryObject<Item> ITEM_COPPER_ROD = ITEMS.register("copper_rod",() -> new Item(new Item.Properties().group(ModItemGroups.MOD_ITEM_GROUP)));
+    public static final RegistryObject<Item> ITEM_IRON_ROD = ITEMS.register("iron_rod",() -> new Item(new Item.Properties().group(ModItemGroups.MOD_ITEM_GROUP)));
+    public static final RegistryObject<Item> ITEM_COPPER_WIRE = ITEMS.register("copper_wire",() -> new Item(new Item.Properties().group(ModItemGroups.MOD_ITEM_GROUP)));
+
+
+
+
 }
